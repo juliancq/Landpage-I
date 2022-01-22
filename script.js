a581d5c27a358ed2187ef27c69aa792a4a1ee647
Julian Castro <julmdq@live.com.ar>
2022-01-22 17:14:23 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const $menuButton = document.querySelector("#menubtn");
+const $menu = document.querySelector(".navbar");
+const $navItems = document.querySelectorAll("#navlink");
+
+$menuButton.addEventListener("click", (e)=>{
+	$menu.classList.toggle("active-menu");
+})
+
+$navItems.forEach((item)=>{
+
+	item.addEventListener("click", (e)=>{
+
+		$menu.classList.toggle("active-menu");
+	})
+})
